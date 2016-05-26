4483e0fd7a827b270060d7bb1bd527927e88e595
Khourn Khemry <Khemry@khemry.local>
2016-05-26 19:49:28 +0900

    first commit.

---
+++ www/js/app.js
@@ -0,0 +1,38 @@
+var app = ons.bootstrap('myApp', ['onsen']);
+
+app.factory('GetData', function($http) { 
+    return $http.get('data/data.json');
+});
+
+// app.service('MsgService', ['$window', function(win) {
+//    	var msgs = [];
+//    	return function(msg) {
+//     	ons.notification.alert({message: msg});
+//    	};
+// }]);
+
+
+
+app.controller('HomeCtrl', function($scope, GetData){
+	GetData.success(function(data) { 
+	    $scope.basic_info = data.basicInfo;
+	});
+});
+
+app.controller('EducationCtrl', function($scope, GetData){
+	GetData.success(function(data) { 
+	    $scope.all_edu_bg = data.educationBackground;
+	});
+});
+
+app.controller('LanguageCtrl', function($scope, GetData){
+	GetData.success(function(data) { 
+	    $scope.languages = data.language;
+	});
+});
+
+app.controller('AwardCtrl', function($scope, GetData){
+	// GetData.success(function(data) { 
+	//     $scope.languages = data.language;
+	// });
+});
