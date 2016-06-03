e3a6243735b4e85c63bf1424b534f4762bd6978f
Khourn Khemry <Khemry@khemry.local>
2016-06-03 12:05:23 +0900

    [add] more updates.

--- www/js/app.js
+++ www/js/app.js
@@ -4,15 +4,6 @@ app.factory('GetData', function($http) {
     return $http.get('data/data.json');
 });
 
-// app.service('MsgService', ['$window', function(win) {
-//    	var msgs = [];
-//    	return function(msg) {
-//     	ons.notification.alert({message: msg});
-//    	};
-// }]);
-
-
-
 app.controller('HomeCtrl', function($scope, GetData){
 	GetData.success(function(data) { 
 	    $scope.basic_info = data.basicInfo;
@@ -31,14 +22,26 @@ app.controller('LanguageCtrl', function($scope, GetData){
 	});
 });
 
-app.controller('AwardCtrl', function($scope, GetData){
-	// GetData.success(function(data) { 
-	//     $scope.languages = data.language;
-	// });
+app.controller('AwardsCtrl', function($scope, GetData){
+	GetData.success(function(data) { 
+	    $scope.awards = data.awards_scholarships;
+	});
 });
 
 app.controller('PublicationCtrl', function($scope, GetData){
 	GetData.success(function(data) { 
 	    $scope.publications = data.publications;
+	});
+});
+
+app.controller('ExperiencesCtrl', function($scope, GetData){
+	GetData.success(function(data) { 
+	    $scope.experiences = data.experiences;
+	});
+});
+
+app.controller('InterestsCtrl', function($scope, GetData){
+	GetData.success(function(data) { 
+	    $scope.interests = data.interests[0];
 	});
 });
